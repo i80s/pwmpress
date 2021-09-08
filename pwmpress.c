5bf65a6416c4c15f079fd9d929a7b1a4276f11f9
Justin Liu <rssnsj@gmail.com>
2021-09-08 14:08:28 +0800

    添加注释

--- pwmpress.c
+++ pwmpress.c
@@ -40,6 +40,12 @@ static int write_to_file(const char *file, const char *str)
 	return 0;
 }
 
+/**
+ * 参数含义:
+ *  $operate_us: 信号持续时长 (微秒)
+ *  $cycle_us: 信号周期时长 (频率的倒数, 微秒)
+ *  $duty_us: 占空时长 (信号周期内高电平的的时长, 微秒)
+ */
 static void pwm_gpio(const char *file, unsigned operate_us, unsigned cycle_us, unsigned duty_us)
 {
 	int fd, i;
@@ -52,10 +58,12 @@ static void pwm_gpio(const char *file, unsigned operate_us, unsigned cycle_us, u
 	for (i = 0; i < operate_us / cycle_us; i++) {
 		char buf[20];
 
+		/* 输出高电平 */
 		sprintf(buf, "1\n");
 		write(fd, buf, strlen(buf));
 		usleep(duty_us);
 
+		/* 输出低电平 */
 		sprintf(buf, "0\n");
 		write(fd, buf, strlen(buf));
 		usleep(cycle_us - duty_us);
@@ -98,8 +106,8 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	/* Initialize the GPIO port */
 	sprintf(the_path, "/sys/class/gpio/gpio%u/direction", gpio_no);
-	/* Initialize the GPIO */
 	if (stat(the_path, &sb) != 0) {
 		sprintf(the_data, "%u\n", gpio_no);
 		rc = write_to_file("/sys/class/gpio/export", the_data);
@@ -110,9 +118,9 @@ int main(int argc, char *argv[])
 
 	/* Operate the values */
 	sprintf(the_path, "/sys/class/gpio/gpio%u/value", gpio_no);
-	pwm_gpio(the_path, 500*1000, 20000, 2500);
-	usleep(hold_ms*1000);
-	pwm_gpio(the_path, 500*1000, 20000, 1500);
+	pwm_gpio(the_path, 500*1000, 20000, 2500); /* 50Hz, 占空2.5ms, 持续0.5s */
+	usleep(hold_ms*1000); /* 按压XXms */
+	pwm_gpio(the_path, 500*1000, 20000, 1500); /* 50Hz, 占空1.5ms, 持续0.5s */
 
 	return 0;
 }
